ddd76cf5740cacf21422cf8792428af3618b6de2
Marcos Marques <marcos.marques@mosb.com.br>
2025-08-22 16:03:09 -0300

    first configuration

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
+import 'dotenv/config'
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+require('dotenv').config({
+  path: ".env." + process.env.ENVIRONMENT
+})
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
